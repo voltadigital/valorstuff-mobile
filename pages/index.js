5b8054f95293435f676c5e874abd422dd1a96461
EkoDoesntDev <jyeenope@gmail.com>
2022-08-04 10:07:43 -0700

    fuck shit cocowe[fdusdpifilsk

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Tabs from '../components/tabs'
+
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <link rel='stylesheet' href='tailwind.css' ></link>
-      </Head>
-        <main>
-         <div>
-
-         </div>
-         <div className='center flex absolute bottom-0 w-full bg-slate-500 h-14 text-center' >
-            <div className='container block m-auto'>
-              <img></img>
-              <p className='m-auto'>Shop</p>
-            </div>
-            <div className='container block m-auto'>
-              <img></img>
-              <p className='m-auto'>Rank</p>
-            </div>
-            <div className='container block m-auto'>
-              <img></img>
-              <p className='m-auto'>Account</p>
-             </div>
-         </div>
-        </main>
-    </div>
+    <Tabs></Tabs>
   )
 }
