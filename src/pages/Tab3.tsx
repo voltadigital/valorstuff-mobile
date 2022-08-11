1bc19eda5ddb00ad3cad10a8a5616361b8855c16
EkoDoesntDev <42520480+EkoDoesntDev@users.noreply.github.com>
2022-08-11 00:44:02 -0400

    and i am midd ass man

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -7,7 +7,7 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Account</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
